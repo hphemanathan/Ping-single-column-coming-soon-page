b4d0f5141d27acd058b9457b684c1f484a31fcf8
hphemanathan <hphemanathan@gmail.com>
2023-12-13 09:21:25 +0530

    wip

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const button = document.querySelector(`.btn`);
 const email = document.querySelector('input');
 
+const alert = document.createElement('p')
+alert.textContent = 'Please provide a valid email address'
+
 
 function click (event) {
 
     const emailid = email.value
     event.preventDefault();
 
-    if (!emailid.includes("@" & ".")) {
-        console.log ('please enter valid email')
-        alert("please enter valid email");
+    if (!emailid.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+      email.insertAdjacentElement("beforebegin", alert);
     } else {
-        console.log ('thanks')
+      console.log("thanks");
     }
 }
 
